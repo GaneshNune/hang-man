0ed85173e56977530feb98d8918ffdebfeff900e
GaneshNune <ganeshnunes@gmail.com>
2024-06-10 12:54:58 +0530

    Project created

---
+++ script.js
@@ -0,0 +1,111 @@
+const wordElement = document.getElementById("word");
+const wrongLettersElement = document.getElementById("wrong-letters");
+const playAgainButton = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+const finalMessageRevealWord = document.getElementById("final-message-reveal-word");
+const figureParts = document.querySelectorAll(".figure-part");
+
+let selectedWord;
+let playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Fetch a random word from an API
+async function fetchRandomWord() {
+  const response = await fetch("https://random-word-api.herokuapp.com/word");
+  const data = await response.json();
+  selectedWord = data[0];
+}
+
+// Display the word on the screen
+function displayWord() {
+  wordElement.innerHTML = `
+    ${selectedWord
+      .split("")
+      .map(
+        (letter) => `
+    <span class="letter">
+    ${correctLetters.includes(letter) ? letter : ""}
+    </span>
+    `
+      )
+      .join("")} 
+    `;
+  const innerWord = wordElement.innerText.replace(/\n/g, "");
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "Congratulations! You won! 😃";
+    finalMessageRevealWord.innerText = "";
+    popup.style.display = "flex";
+    playable = false;
+  }
+}
+
+// Update the wrong letters element
+function updateWrongLettersElement() {
+  wrongLettersElement.innerHTML = `
+  ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+  ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+    index < errors ? (part.style.display = "block") : (part.style.display = "none");
+  });
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "Unfortunately you lost. 😕";
+    finalMessageRevealWord.innerText = `...the word was: ${selectedWord}`;
+    popup.style.display = "flex";
+    playable = false;
+  }
+}
+
+// Show notification
+function showNotification() {
+  notification.classList.add("show");
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+// Handle keypress events
+window.addEventListener("keypress", (e) => {
+  if (playable) {
+    const letter = e.key.toLowerCase();
+    if (letter >= "a" && letter <= "z") {
+      if (selectedWord.includes(letter)) {
+        if (!correctLetters.includes(letter)) {
+          correctLetters.push(letter);
+          displayWord();
+        } else {
+          showNotification();
+        }
+      } else {
+        if (!wrongLetters.includes(letter)) {
+          wrongLetters.push(letter);
+          updateWrongLettersElement();
+        } else {
+          showNotification();
+        }
+      }
+    }
+  }
+});
+
+// Handle play again button click
+playAgainButton.addEventListener("click", async () => {
+  playable = true;
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+  await fetchRandomWord();
+  displayWord();
+  updateWrongLettersElement();
+  popup.style.display = "none";
+});
+
+// Init
+(async function init() {
+  await fetchRandomWord();
+  displayWord();
+})();
